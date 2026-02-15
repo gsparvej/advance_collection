a497500675906b53932d3c566e2da87b538fd852
parvej <parvejmd4469@gmail.com>
2026-02-15 12:29:51 +0600

    feat: Introduce Doctor Visit Entry component and integrate it into App.js, replacing ProcedureEntry.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import ProcedureEntry from './procedure/procedure_entry';
+// import ProcedureEntry from './procedure/procedure_entry';
 // import AdvanceCollection from './advance/advance_collection';
 // import ExtraBed from './extraBed/extra_bed';
 // import BedChange from './bedChange/bed_change';
+import DoctorVisitEntry from './doctorVisit/doctor_visit';
 function App() {
   return (
     <div className="App">
       {/* <AdvanceCollection /> */}
       {/* <ExtraBed /> */}
       {/* <BedChange /> */}
-      <ProcedureEntry />
+      {/* <ProcedureEntry /> */}
+      <DoctorVisitEntry />
 
     </div>
   );
